927c8224433e4f246b04aaa2e10ea8d871859073
Zalimladka <159771766+Zalimladka@users.noreply.github.com>
2025-03-17 11:50:07 +0530

    Update fb_dtsg_extractor.py

--- fb_dtsg_extractor.py
+++ fb_dtsg_extractor.py
@@ -1,37 +1,16 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-import json
 
-# Load cookies
-with open('cookies.json', 'r') as f:
-    cookies = json.load(f)
-
+# Chrome options
 chrome_options = Options()
 chrome_options.binary_location = "/usr/bin/chromium-browser"
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-# Initialize the browser
-driver = webdriver.Chrome(options=chrome_options)
+# Driver setup
+driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=chrome_options)
+driver.get('https://www.facebook.com')
 
-# Open Facebook
-driver.get("https://www.facebook.com")
-
-# Add cookies
-for cookie_name, cookie_value in cookies.items():
-    driver.add_cookie({"name": cookie_name, "value": cookie_value})
-
-# Refresh the page to apply cookies
-driver.refresh()
-
-# Extract fb_dtsg
-fb_dtsg = driver.execute_script("return document.querySelector('[name=\\\"fb_dtsg\\\"]').value")
-
-if fb_dtsg:
-    print("[+] fb_dtsg:", fb_dtsg)
-else:
-    print("[-] fb_dtsg Not Found")
-
-# Close the browser
-driver.quit()
+print(driver.page_source)
