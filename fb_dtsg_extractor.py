876c03121d5d24e1276c1fc2034d58e956d913d9
Zalimladka <159771766+Zalimladka@users.noreply.github.com>
2025-03-17 12:00:07 +0530

    Update fb_dtsg_extractor.py

--- fb_dtsg_extractor.py
+++ fb_dtsg_extractor.py
@@ -4,9 +4,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 chrome_options = Options()
-chrome_options.binary_location = "/usr/bin/chromium-browser"
+chrome_options.binary_location = "/usr/bin/google-chrome"
 chrome_options.add_argument("--headless")  # Background mode
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+print("[+] Setting up ChromeDriver")
+
+try:
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    print("[+] ChromeDriver initialized successfully")
+except Exception as e:
+    print(f"[-] Error initializing ChromeDriver: {e}")
