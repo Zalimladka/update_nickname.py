b9e3d3dadf7e1e57a85bfa09b8d594c51be3fa3a
Zalimladka <159771766+Zalimladka@users.noreply.github.com>
2025-03-17 11:58:06 +0530

    Update fb_dtsg_extractor.py

--- fb_dtsg_extractor.py
+++ fb_dtsg_extractor.py
@@ -1,16 +1,12 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
-# Chrome options
 chrome_options = Options()
 chrome_options.binary_location = "/usr/bin/chromium-browser"
-chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")  # Background mode
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-# Driver setup
-driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=chrome_options)
-driver.get('https://www.facebook.com')
-
-print(driver.page_source)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
