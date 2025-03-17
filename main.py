454e537fea79128c9f3e3169f78dd0bc2b5974ec
Zalimladka <159771766+Zalimladka@users.noreply.github.com>
2025-03-17 17:33:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
-import requests
-import random
-
-def get_free_proxies():
-    url = "https://www.proxyscan.io/api/proxy?type=http,https"
-    response = requests.get(url).json()
-    return [f"{proxy['Ip']}:{proxy['Port']}" for proxy in response]
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 chrome_options = Options()
 chrome_options.binary_location = "/usr/bin/chromium-browser"
-chrome_options.add_argument("--headless")  # Background mode
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-proxies = get_free_proxies()
-proxy = random.choice(proxies)
-
-proxy_dict = {
-    'http': f'http://{proxy}',
-    'https': f'http://{proxy}'
-}
-
-print("[+] Setting up ChromeDriver")
-
-try:
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-    print("[+] ChromeDriver initialized successfully")
-    response = requests.get('https://facebook.com', proxies=proxy_dict)
-    print(response.text)
-except Exception as e:
-    print(f"[-] Error: {e}")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
